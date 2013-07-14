57f71f0e6900288dc0148ba95ac30df767ec9e59
caijinsol <caijinsol@gmail.com>
2013-07-14 09:07:04 -0400

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Healthcon::Application.config.secret_token = '3d2876e0714a6f7eda35b835b8d90e6cd330760f432f505680f5964975fac8130e640d2bb568f974f5706013b48c3e94ea37271b3787824b3f89a40278b766f7'
